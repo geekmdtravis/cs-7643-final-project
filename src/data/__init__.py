00eb6bdeab67ce4ead3def243dd3f5becc96dd0f
geekmdtravis <travis@geekmd.io>
2025-04-15 03:00:13 -0300

    prepare data provisionally working to download, split, and embed

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,4 +1,4 @@
 from .create_dataloaders import create_dataloaders
 from .dataset import ChestXrayDataset
 from .download import download_dataset
-from .process_data import process_data
+from .prepare_data import process_data
