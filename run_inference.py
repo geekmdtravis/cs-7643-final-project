742444b74f139f6279dd54da4af97687928ee846
Emad Albalas <emadalbalas@gmail.com>
2025-04-22 11:04:15 -0700

    corrected run_inference so it can run properly

--- run_inference.py
+++ run_inference.py
@@ -4,14 +4,14 @@ from src.utils import Config, evaluate_model, print_evaluation_results, run_infe
 
 cfg = Config()
 
-model = CXRModel(model="densenet121", freeze_backbone=True, hidden_dims=())
+model = CXRModel(model="densenet121", freeze_backbone=True)
 
 path = "results/models/best_model_densenet121.pth"
 loader = create_dataloader(
     clinical_data=cfg.tabular_clinical_test,
     cxr_images_dir=cfg.cxr_test_dir,
     num_workers=32,
-    batch_size=32,
+    batch_size=16,
     normalization_mode="imagenet",
 )
 preds, labels = run_inference(
@@ -21,9 +21,11 @@ preds, labels = run_inference(
     device="cuda",
 )
 
+# Get evaluation results
+results = evaluate_model(preds, labels)
 
-auc_scores, report = evaluate_model(preds, labels)
-
+# Print and save results
 print_evaluation_results(
-    auc_scores=auc_scores, report=report, save_path="results/evaluation_report.txt"
+    results=results,
+    save_path="results/evaluation_report.txt",
 )
