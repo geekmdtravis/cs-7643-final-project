945e5f25037355b94634bb057817093d7e343a2b
geekmdtravis <travis@geekmd.io>
2025-04-30 10:26:53 -0700

    fixed linting errors

--- experiment11_ablation.py
+++ experiment11_ablation.py
@@ -23,7 +23,7 @@ def main(model_path):
     model.load_state_dict(save_info["model"])
     model.to(device)
 
-    print('Model Successfully Loaded')
+    print("Model Successfully Loaded")
 
     model.eval()
 
@@ -38,23 +38,20 @@ def main(model_path):
 
     preds, labels = run_inference(model=model, test_loader=loader)
 
-    ablated_results.append({
-        'Feature Index': '5',
-        'Feature Name': 'All',
-        'Predictions': preds,
-        'Labels': labels
-    })
+    ablated_results.append(
+        {
+            "Feature Index": "5",
+            "Feature Name": "All",
+            "Predictions": preds,
+            "Labels": labels,
+        }
+    )
 
     print("Baseline AUROCs added")
 
     tabular_features = ["Age", "Gender", "View Position", "Follow Up #"]
 
-    ablated_feature_values = {
-        0: 0.5,
-        1: 0.5,
-        2: 0.5,
-        3: 0
-    }
+    ablated_feature_values = {0: 0.5, 1: 0.5, 2: 0.5, 3: 0}
 
     actual_forward = model.forward
 
@@ -78,33 +75,33 @@ def main(model_path):
 
         model.forward = actual_forward  # Restore model.forward
 
-        ablated_results.append({
-            'Feature Index': feat_idx,
-            'Feature Name': feat_name,
-            'Predictions': preds,
-            'Labels': labels
-        })
+        ablated_results.append(
+            {
+                "Feature Index": feat_idx,
+                "Feature Name": feat_name,
+                "Predictions": preds,
+                "Labels": labels,
+            }
+        )
 
-
-    auroc_results=[]
+    auroc_results = []
 
     class_names = [name for name in cfg.class_labels]
 
     tabular_features = ["None", "Age", "Gender", "View Position", "Follow Up #"]
 
     for results in ablated_results:
-        feature_name = results['Feature Name']
+        feature_name = results["Feature Name"]
         print("Calculating Ablated AUROCs for", feature_name)
         if feature_name == "All":
             feature_name = "None"
-        labels = results['Labels']
-        preds = results['Predictions']
+        labels = results["Labels"]
+        preds = results["Predictions"]
 
         thresholds = find_optimal_thresholds(labels, preds, cfg.class_labels)
         binary_preds = np.zeros_like(preds)
         for i, label in enumerate(cfg.class_labels):
             binary_preds[:, i] = (preds[:, i] >= thresholds[label]).astype(int)
-
 
         class_aurocs = []
         # We will calculate One vs All AUROC
@@ -119,7 +116,6 @@ def main(model_path):
 
         auroc_results.append((feature_name, class_aurocs))
 
-
     auroc_df = pd.DataFrame(index=class_names, columns=tabular_features)
     for feature_name, class_aurocs in auroc_results:
         for class_name, auroc in class_aurocs:
@@ -128,23 +124,28 @@ def main(model_path):
     auroc_df = auroc_df.astype(float)
     auroc_df_transposed = auroc_df.T
 
-    auroc_df_diff = auroc_df_transposed.subtract(auroc_df_transposed.loc['None'], axis=1)
-
-
+    auroc_df_diff = auroc_df_transposed.subtract(
+        auroc_df_transposed.loc["None"], axis=1
+    )
 
     plt.figure(figsize=(15, 8))
-    sns.heatmap(auroc_df_diff, annot=auroc_df_transposed, cmap="Spectral", fmt=".3f", cbar=False)
-    plt.title('Ablation Study', fontsize=16)
-    plt.ylabel('Ablated Feature', fontsize=14)
-    plt.xlabel('Class', fontsize=18)
+    sns.heatmap(
+        auroc_df_diff, annot=auroc_df_transposed, cmap="Spectral", fmt=".3f", cbar=False
+    )
+    plt.title("Ablation Study", fontsize=16)
+    plt.ylabel("Ablated Feature", fontsize=14)
+    plt.xlabel("Class", fontsize=18)
 
     ax = plt.gca()
-    ax.set_xticklabels(ax.get_xticklabels(), rotation=30, ha='right') #https://stackoverflow.com/questions/14852821/aligning-rotated-xticklabels-with-their-respective-xticks
+    ax.set_xticklabels(
+        ax.get_xticklabels(), rotation=30, ha="right"
+    )  # https://stackoverflow.com/questions/14852821/aligning-rotated-xticklabels-with-their-respective-xticks
 
     plt.tight_layout()
 
-    plt.savefig('ablation_study_heatmap.png', dpi =300)
+    plt.savefig("ablation_study_heatmap.png", dpi=300)
     plt.show()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
